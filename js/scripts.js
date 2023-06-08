36432e23ae9b1a8bb2baf279917c9eb20b6d78d9
Schmelzberry <jelsberry96@gmail.com>
2023-06-08 11:58:17 -0700

    Co-authored-by: Sage Paden <sagepaden@users.noreply.github.com> Co-authored-by: Stella Marie <SmKou@users.noreply.github.com> Co-authored-by: Thomas Bakken <tbakken14@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+/**
+ * Checks if a number is a valid credit card number.
+ * @param {String} cardNumber
+ * @returns {String}
+ */
+function isCreditCardValid(cardNumber) {
+    if (cardNumber == "" || cardNumber.length <= 14 || cardNumber.length >= 17)
+        return "This card number is not valid.";
+    let sum = 0;
+    for (let i = 0; i < cardNumber.length; i++) {
+        let c = parseInt(cardNumber.charAt(i));
+        if (i & 1) { // odd
+            c *= 2;
+            if (c > 9) 
+                c = 1 + (c % 10);
+        }
+        sum += c;
+    }
+    if (sum % 10 != 0)
+        return "This card number is not valid.";
+    return "This card number is valid.";
+}
+
+/*
+sum += (i & 1)?
+    (c * 2 > 9)? 
+        1 + (c * 2 % 10)
+        : c * 2
+    : c
+*/
+
+let num = "";
+console.log(num, isCreditCardValid(num));
+num = "71516394042069";
+console.log(num, isCreditCardValid(num));
+num = "71516394042069000";
+console.log(num, isCreditCardValid(num));
+num = "4102080860435620";
+console.log(num, isCreditCardValid(num));
+num = "4102080880435620";
+console.log(num, isCreditCardValid(num));
